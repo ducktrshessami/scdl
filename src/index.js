ca61a49431b38ba9401a80b63a52ea14d52d3526
ducktrshessami <jwee761@gmail.com>
2020-11-20 02:37:22 -0800

    restructure for proper global installation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,84 @@
 const scdl = require("scdl-core"); // SoundCloud
-const fs = require("fs"); // File I/O
+const fs = require("fs").promises; // File I/O
 
-scdl.setClientID(""); // Set client_id
+var URL, clientID, OAuth;
 
-const URL = process.argv[2]; // Get arg
-if (!URL) {
-    throw "URL not specified";
+/*
+Handle args
+*/
+function main() {
+    for (let i = 2; i < process.argv.length; i++) {
+        switch (process.argv[i].toLowerCase()) {
+            case "-c":
+            case "--client-id":
+                if (i == process.argv.length - 1) {
+                    throw "client_id not specified";
+                }
+                else {
+                    clientID = clientID || process.argv[++i];
+                }
+                break;
+            case "-a":
+            case "--oauth-token":
+                if (i == process.argv.length - 1) {
+                    throw "OAuth token not specified";
+                }
+                else {
+                    OAuth = OAuth || process.argv[++i];
+                }
+                break;
+            default:
+                URL = URL || process.argv[i];
+                break;
+        }
+    }
+    if (clientID) {
+        newClientID(clientID);
+    }
+    if (OAuth) {
+        newOauthToken(OAuth);
+    }
+    if (clientID || OAuth) {
+        updateConfig();
+    }
+    if (URL) {
+        downloadSong(URL);
+    }
+    if (!(URL || clientID || OAuth)) {
+        throw "URL not specified";
+    }
 }
-if (!scdl.validateURL(URL)) {
-    throw "Invalid URL: " + URL;
+
+/*
+*/
+async function downloadSong(URL) {
+    if (!scdl.validateURL(URL)) {
+        throw "Invalid URL: " + URL;
+    }
+    else {
+        scdl.getInfo(URL).then(info => { // Get title for filename
+            console.log("Downloading " + info.title);
+            scdl.downloadFromInfo(info).pipe(fs.createWriteStream(info.title + ".mp3")); // Download to file
+        }).catch(console.error);
+    }
 }
 
-scdl.getInfo(URL).then(info => { // Get title for filename
-    console.log("Downloading " + info.title);
-    scdl.downloadFromInfo(info).pipe(fs.createWriteStream(info.title + ".mp3")); // Download to file
-}).catch(console.log);
+/*
+*/
+function newClientID(ID) {
+    scdl.setClientID(ID);
+}
+
+/*
+*/
+function newOauthToken(token) {
+    scdl.setOauthToken(token);
+}
+
+/*
+*/
+function updateConfig() {
+    
+}
+
+main();
