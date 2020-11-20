d96c93ff359eec6aefe0d094d9ea7f24b10f5028
ducktrshessami <jwee761@gmail.com>
2020-11-20 03:37:59 -0800

    tidy up

--- bin/index.js
+++ bin/index.js
@@ -4,7 +4,7 @@ const scdl = require("scdl-core"); // SoundCloud
 const fs = require("fs").promises; // File I/O
 var config = require("../cfg/config.json"); // Authorization
 
-var URL, ID, OAuth;
+var query, ID, OAuth;
 
 /*
 Handle args and config updates
@@ -12,7 +12,7 @@ Handle args and config updates
 function main() {
     for (let i = 2; i < process.argv.length; i++) {
         switch (process.argv[i].toLowerCase()) {
-            case "-c":
+            case "-c": // Set client_id
             case "--client-id":
                 if (i == process.argv.length - 1) {
                     throw "client_id not specified";
@@ -21,7 +21,7 @@ function main() {
                     ID = ID || process.argv[++i];
                 }
                 break;
-            case "-a":
+            case "-a": // Set OAuth token
             case "--oauth-token":
                 if (i == process.argv.length - 1) {
                     throw "OAuth token not specified";
@@ -30,39 +30,42 @@ function main() {
                     OAuth = OAuth || process.argv[++i];
                 }
                 break;
-            default:
-                URL = URL || process.argv[i];
+            default: // Set download query
+                query = query || process.argv[i];
                 break;
         }
     }
-    if (ID) {
+    if (ID) { // Handle setting authorization like this to get first set if multiple
         config.clientID = ID;
     }
     if (OAuth) {
         config.oauthToken = token;
     }
-    if (ID || OAuth) {
+    if (ID || OAuth) { // Update stored config
         fs.writeFile(`${__dirname}/../cfg/config.json`, JSON.stringify(config, null, 4)).catch(console.error);
     }
-    if (URL) {
-        downloadSong(URL);
+    if (query) { // Got query at some point
+        downloadSong(query);
     }
-    if (!(URL || ID || OAuth)) {
+    if (!(query || ID || OAuth)) { // Got nothing
         throw "URL not specified";
     }
 }
 
 /*
+Download song from CLI param
+
+@param URL: a string containing the supposed URL to download
 */
-async function downloadSong(URL) {
+async function downloadSong(query) {
     scdl.setClientID(config.clientID); // Set authorization
     scdl.setOauthToken(config.oauthToken);
 
-    if (!scdl.validateURL(URL)) {
-        throw "Invalid URL: " + URL;
+    if (!scdl.validateURL(query)) {
+        throw "Invalid URL: " + query;
     }
     else {
-        scdl.getInfo(URL).then(info => { // Get title for filename
+        scdl.getInfo(query).then(info => { // Get title for filename
             console.log("Downloading " + info.title);
             scdl.downloadFromInfo(info).pipe(fs.createWriteStream(info.title + ".mp3")); // Download to file
         }).catch(console.error);
