276b54a601d784d27a1bdb45bc8ddc242fef2bfe
Josh Fish <jwee761@gmail.com>
2022-07-26 16:56:19 -0700

    arg parsing

--- bin/parseArgs.js
+++ bin/parseArgs.js
@@ -1,24 +1,62 @@
 const flags = {
-    p: {
-        long: "playlist",
+    playlist: {
+        short: "p",
         requireValue: false
     },
-    o: {
-        long: "output",
+    output: {
+        short: "o",
         requireValue: true
     },
-    c: {
-        long: "client-id",
+    "client-id": {
+        short: "c",
         requireValue: true
     },
-    a: {
-        long: "oauth-token",
+    "oauth-token": {
+        short: "a",
         requireValue: true
     }
 };
 
-function parseArgs() {
+function getFlag(str) {
+    if (str) {
+        const name = str
+            .match(/^(?:--?)([\w-]+)$/)
+            ?.at(1);
+        return Object
+            .keys(flags)
+            .find(flag => flag === name || flags[flag].short === name);
+    }
+}
 
+function parseArgs() {
+    const result = {};
+    Object
+        .keys(flags)
+        .forEach(flag => {
+            if (!flags[flag].requireValue) {
+                result[flag] = false;
+            }
+        });
+    for (let i = 2; i < process.argv.length; i++) {
+        const flag = getFlag(process.argv[i]);
+        if (flag) {
+            if (flags[flag].requireValue) {
+                if (!process.argv[i + 1] || getFlag(process.argv[i + 1])) {
+                    throw new Error(`Option ${process.argv[i]} requires a value`);
+                }
+                else {
+                    result[flag] = process.argv[++i];
+                }
+            }
+            else {
+                result[flag] = true;
+            }
+        }
+        else if (!result.query) {
+            result.query = process.argv[i];
+        }
+    }
+    return result;
 }
 
 module.exports = parseArgs;
