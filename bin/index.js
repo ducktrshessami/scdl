f5bb728d2d3487dfc8d463e58b0315371f3fc655
Josh Fish <jwee761@gmail.com>
2022-07-26 18:31:52 -0700

    help text display

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,8 @@
 
 const scdl = require("scdl-core");
 const { fetchKey } = require("soundcloud-key-fetch");
+const path = require("path");
+const fs = require("fs");
 const config = require("./config");
 const parseArgs = require("./parseArgs");
 
@@ -13,28 +15,43 @@ async function main() {
         "oauth-token": argsOauthToken,
         output
     } = parseArgs();
+    let hadAction = false;
     scdl.clientID = clientID;
     scdl.oauthToken = argsOauthToken;
     if (argsOauthToken) {
+        hadAction = true;
         console.log("Storing Oauth token");
         config.write(argsOauthToken);
     }
-    if (playlist ? scdl.playlist.validateURL(query) : scdl.validateURL(query)) {
-        if (!scdl.clientID && !scdl.oauthToken) {
-            const { oauthToken: configOauthToken } = config.read();
-            if (configOauthToken) {
-                scdl.oauthToken = configOauthToken;
+    if (query) {
+        hadAction = true;
+        if (playlist ? scdl.playlist.validateURL(query) : scdl.validateURL(query)) {
+            if (!scdl.clientID && !scdl.oauthToken) {
+                const { oauthToken: configOauthToken } = config.read();
+                if (configOauthToken) {
+                    scdl.oauthToken = configOauthToken;
+                }
+                else {
+                    console.log("Fetching client ID");
+                    scdl.clientID = await fetchKey();
+                }
             }
-            else {
-                console.log("Fetching client ID");
-                scdl.clientID = await fetchKey();
-            }
+            return (playlist ? downloadPlaylist : downloadTrack)(query, output);
         }
-        return (playlist ? downloadPlaylist : downloadTrack)(query, output);
+        else {
+            throw new Error("Invalid URL");
+        }
     }
-    else {
-        throw new Error("Invalid URL");
+    if (!hadAction) {
+        displayHelp();
     }
+}
+
+function displayHelp() {
+    const usagePath = path.resolve(__dirname, "..", "USAGE");
+    fs
+        .createReadStream(usagePath, { encoding: "utf8" })
+        .pipe(process.stdout);
 }
 
 async function downloadTrack(query, output) {
