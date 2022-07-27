d1fdc54ac332d78f3c1d7d9bc8f2a7922cb6dfa7
Josh Fish <jwee761@gmail.com>
2022-07-26 17:44:46 -0700

    main handling

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,45 @@
 #!/usr/bin/env node
+
+const scdl = require("scdl-core");
+const { fetchKey } = require("soundcloud-key-fetch");
+const config = require("./config");
+const parseArgs = require("./parseArgs");
+
+async function main() {
+    const {
+        query,
+        playlist,
+        "client-id": clientID,
+        "oauth-token": argsOauthToken,
+        output
+    } = parseArgs();
+    scdl.clientID = clientID;
+    scdl.oauthToken = argsOauthToken;
+    if (playlist ? scdl.playlist.validateURL(query) : scdl.validateURL(query)) {
+        if (!scdl.clientID && !scdl.oauthToken) {
+            const { oauthToken: configOauthToken } = config.read();
+            if (configOauthToken) {
+                scdl.oauthToken = configOauthToken;
+            }
+            else {
+                console.log("Fetching client ID");
+                scdl.clientID = await fetchKey();
+            }
+        }
+        return (playlist ? downloadPlaylist : downloadTrack)(query, output);
+    }
+    else {
+        throw new Error("Invalid URL");
+    }
+}
+
+async function downloadPlaylist(query, output) {
+
+}
+
+async function downloadTrack(query, output) {
+
+}
+
+main()
+    .catch(console.error);
