23936c5a3adfafe0e41f6a6a59199ff3d51d2ef0
Josh Fish <jwee761@gmail.com>
2022-07-27 15:58:58 -0700

    get info invalid oauth handle general

--- bin/index.js
+++ bin/index.js
@@ -36,7 +36,8 @@ async function main() {
                     scdl.clientID = await fetchKey();
                 }
             }
-            return (playlist ? downloadPlaylist : downloadTrack)(query, output);
+            const info = await getInfoWithRetry(query, playlist);
+            return (playlist ? downloadPlaylist : downloadTrack)(info, output);
         }
         else {
             throw new Error(`Invalid URL: ${query}`);
@@ -54,6 +55,24 @@ function displayHelp() {
         .pipe(process.stdout);
 }
 
+async function getInfoWithRetry(url, playlist) {
+    try {
+        return await (playlist ? scdl.playlist.getInfo : scdl.getInfo)(url);
+    }
+    catch (error) {
+        if (scdl.oauthToken && error.message === "401 Unauthorized") {
+            console.log("Invalid OAuth token\nClearing token and fetching client ID");
+            await config.write();
+            scdl.oauthToken = null;
+            scdl.clientID ||= await fetchKey();
+            return getInfoWithRetry(url, playlist);
+        }
+        else {
+            console.error(error);
+        }
+    }
+}
+
 function generateFilename(info, outputDir = path.resolve(process.cwd()), n = 0) {
     let i = 0;
     let filename = `${info.user.username}-${info.title}-${info.id}.mp3`;
@@ -63,32 +82,22 @@ function generateFilename(info, outputDir = path.resolve(process.cwd()), n = 0) 
     return filename;
 }
 
-async function downloadTrack(query, output) {
-    try {
-        const info = await scdl.getInfo(query);
+function downloadTrack(info, output) {
+    return new Promise(resolve => {
         const outputPath = path.resolve(output || generateFilename(info));
         console.log(`Streaming to ${outputPath}`);
         scdl
             .downloadFromInfo(info)
             .on("error", console.error)
-            .on("end", () => console.log("Done"))
+            .on("end", () => {
+                console.log("Done");
+                resolve();
+            })
             .pipe(fs.createWriteStream(outputPath));
-    }
-    catch (error) {
-        if (scdl.oauthToken && error.message === "401 Unauthorized") {
-            console.log("Invalid OAuth token\nClearing token and fetching client ID");
-            await config.write();
-            scdl.oauthToken = null;
-            scdl.clientID ||= await fetchKey();
-            return downloadTrack(query, output);
-        }
-        else {
-            console.error(error);
-        }
-    }
+    });
 }
 
-async function downloadPlaylist(query, output) {
+async function downloadPlaylist(info, output) {
 
 }
 
